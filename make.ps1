9a5e6664defdbd90d62076dcba118f27e1809309
Frederico <fredeschpe@gmail.com>
2023-02-04 22:52:44 -0300

    changed order of compilation because it makes more sense the way it is

--- make.ps1
+++ make.ps1
@@ -1,5 +1,5 @@
 cl /std:c11 /W4 /WX /I include `
-    .\src\main.c .\src\gl.c `
+    .\src\gl.c .\src\main.c `
     /Fe"main.exe" `
     /link .\dependencies\glfw3dll.lib opengl32.lib User32.lib Gdi32.lib Shell32.lib
 
